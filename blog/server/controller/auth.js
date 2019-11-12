d953e3ca811c45571197048f1f35a27cb9d4e7ee
Antonio1718 <nirinahantonio@gmail.com>
2019-11-12 08:53:52 +0300

    run_signin

--- blog/server/controller/auth.js
+++ blog/server/controller/auth.js
@@ -1,3 +1,8 @@
+require('dotenv').config();
+const bcrypt = require('bcrypt-nodejs');
+const secret              = process.env.SECRET;
+const jwt                 = require('jsonwebtoken');
+const User = require('../model/user');
 
 module.exports.isAuthorized  = function(req, res, next) {
 	console.log(req.headers)
@@ -10,4 +15,91 @@ module.exports.isAuthorized  = function(req, res, next) {
 	}
 	
 	next();
+}
+
+/**
+ * Sign up a new user
+ *
+ * @param req
+ * @param res
+ * @param next
+ */
+exports.signup = function(req, res, next) {
+
+  // console.log(req.body);
+  const email = req.body.email;
+  const password = req.body.password;
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const address = req.body.address;
+  const phone = req.body.phone;
+
+  if (!email || !password) {
+    return res.status(400).send({ message: 'You must provide both email and password.' }); 
+  } 
+
+  // See if a user with given email exists
+  User.findOne({ email: email }, function(err, existingUser) {
+
+    if (err) {
+      return next(err);
+    }
+
+    // If a user with email does exist, return an error
+    if (existingUser) {
+      return res.status(400).send({ message: 'This email is in use.' });  // 422 refers to unprocessable entity
+    }
+
+    // If a user with email does NOT exist, create and save user record
+    const user = new User({
+      email: email,
+      password: password,
+      firstName: firstName,
+      lastName: lastName,
+      phone:phone,
+      address:address
+    });
+
+    user.save(function(err) {  // callback function
+      if (err) {
+        return next(err);
+      }
+
+      // Respond user request indicating the user was created
+      res.json({ message: 'You have successfully signed up. You can sign in now.' });
+    });
+  });
+};
+
+/**
+ * create private_key and public_key
+ * openssl genpkey -out config/private.pem -aes256 -algorithm rsa -pkeyopt rsa_keygen_bits:4096
+ * openssl pkey -in config/private.pem -out config/jwt/public.pem -pubout
+ *
+ * @param req
+ * @param res
+ * @param next
+ */
+exports.signin = function(req, res, next) {
+	const email = req.body.username;
+	const payload = {    
+    	email:email
+    };
+	User.findOne({ email: email }, function(err, resp) {
+		if (err) {
+			return res.status(400).send({ message: 'This email not found.' });
+		}
+		const checkPwd = bcrypt.compareSync(req.body.password,resp.password);
+
+		if (checkPwd) {
+            jwt.sign(payload, secret, {expiresIn: 3600 }, (err, token) => {
+                //console.log(jwt.decode(token))
+                res.json({
+                    success : true,
+                    code_status : 200,
+                    data: token
+                }); 
+            });
+        }
+	})
 }
--- blog/server/server.js
+++ blog/server/server.js
@@ -6,21 +6,21 @@ const app = express();
 const router = require('./route');
 const mongoose = require('mongoose');
 const cors = require('cors'); 
-// const router = express.Router();
 // DB Setup (connect mongoose and instance of mongodb)
 // sudo systemctl status mongodb
-// mongoose.connect('mongodb://localhost:blog/blog');
+// mongoose.connect('mongodb://username:password@host:port/database?options...', {useNewUrlParser: true});
+mongoose.connect('mongodb://localhost:27017/blog', {useNewUrlParser: true});
 
 // App Setup (morgan and body-parser are middleware in Express)
 app.use(morgan('combined'));  // middleware for logging
 app.use(bodyParser.json({ type: '*/*' }));  // middleware for helping parse incoming HTTP requests
-app.use(cors());  // middleware for circumventing (规避) cors error
+app.use(cors());  
 
 // Router Setup
 router(app);
 
 // Server Setup
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 const server = http.createServer(app);
 server.listen(port);
 console.log('Server listening on: ', port);

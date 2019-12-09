7ba68a44a26eca31e8c54ada66570cc219b55682
Antonio1718 <nirinahantonio@gmail.com>
2019-12-09 07:33:53 +0300

    add_printer_algo

--- algo/snake.go
+++ algo/snake.go
@@ -4,36 +4,6 @@ import (
 	"fmt"
 )
 
-func main(){
-	n := 4
-	var ar = []int{31,28,31,30,31,30,31,31,30,31,30,31}
-	factoriel(n)
-	moyen(ar)
-}
-
-func factoriel(n int){
-	f := 1
-	
-	for n != 1{
-		f = n * f
-		n = n - 1
-	}
-
-	fmt.Println("factoriel", f)
-
-}
-
-func moyen(array []int) {
-	count := len(array)
-	moy := 0
-
-	for i:= 0; i < count; i++ {
-		moy = array[i] + moy
-	}
-
-	fmt.Println("moyen du mois", moy / count)
-}
-
 func printer(){
 	for i := 1; i <= 20; i++ {
 		for j := 1; j <= 50; j++ {
